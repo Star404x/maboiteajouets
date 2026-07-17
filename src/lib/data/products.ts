dc5892a8ca3c06d0560087faf6986c4545bcea37
Star404x <dev@maboiteajouets.fr>
2026-07-17 17:01:41 +0000

    Force redeploy with correct env variables

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -1,7 +1,7 @@
 import type { Product } from "@/lib/types";
 
 /**
- * Синхронизировано из БД: 2026-07-17T16:49:37.254Z
+ * Синхронизировано из БД: 2026-07-17T16:58:07.832Z
  * Всего товаров: 25
  */
 export const PRODUCTS: Product[] = [
